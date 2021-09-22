5f5e865ca5a6108442db1eb2d60cb20cc713b11c
Ryan Shnitman <rshnitman@gmail.com>
2021-09-22 16:55:59 -0400

    minor visual updates

--- tests.py
+++ tests.py
@@ -4,26 +4,26 @@ from sudoku import Sudoku
 from copy import deepcopy
 
 solvable_board = Sudoku(['530070000',
-                             '600195000',
-                             '098000060',
-                             '800060003',
-                             '400803001',
-                             '700020006',
-                             '060000280',
-                             '000419005',
-                             '000080079'])
+                         '600195000',
+                         '098000060',
+                         '800060003',
+                         '400803001',
+                         '700020006',
+                         '060000280',
+                         '000419005',
+                         '000080079'])
 
 solvable_board_original = deepcopy(solvable_board)
 
 unsolvable_board = Sudoku(['516849732',
-                               '307605000',
-                               '809700065',
-                               '135060907',
-                               '472591006',
-                               '968370050',
-                               '253186074',
-                               '684207500',
-                               '791050608'])
+                           '307605000',
+                           '809700065',
+                           '135060907',
+                           '472591006',
+                           '968370050',
+                           '253186074',
+                           '684207500',
+                           '791050608'])
 
 unsolvable_board_original = deepcopy(unsolvable_board)
 
@@ -49,7 +49,6 @@ class TestSolve(unittest.TestCase):
 
     def test_unsolvable_board(self):
         self.assertEqual(unsolvable_board.board, unsolvable_board_original.board, "Should be unaltered")
-
 
 
 if __name__ == '__main__':
